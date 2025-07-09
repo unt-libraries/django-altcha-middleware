7103eff45246a8f1626af06e805003a9f116a5a6
Gio Gottardi <manuelgottardi@unt.edu>
2025-07-09 16:31:33 -0500

    Fill out README and LICENSE, adjust requirements and setup.py, and complete tox.ini.

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     description='Django app for adding altcha proof-of-work challenges to views.',
     long_description='See the home page for more information.',
     include_package_data=True,
+    install_requires=[
+        'django >=4.2, <=5.2',
+        'altcha ~= 0.2.0',
+    ]
+    url='https://github.com/unt-libraries/django-altcha-middleware',
     author='University of North Texas Libraries',
     author_email='mark.phillips@unt.edu',
     license='BSD',
@@ -14,11 +19,16 @@ setup(
     classifiers=[
         'Natural Language :: English',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ]
 )
